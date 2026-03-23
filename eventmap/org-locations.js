1b94d6aee13e378ceead2125a3eecb62fe8bce20
Caterina Fuster-Barceló <caterina.fusterbarcelo@mls.uzh.ch>
2026-03-23 10:07:56 +0100

    adding map to events

---
+++ eventmap/org-locations.js
@@ -0,0 +1,11 @@
+var addressPoints = [
+  ["Madrid, Spain", 40.4168, -3.7038],
+  ["Granada, Spain", 37.1765, -3.5979],
+  ["Stockholm, Sweden", 59.3293, 18.0686],
+  ["Milan, Italy", 45.4642, 9.19],
+  ["Heidelberg, Germany", 49.3988, 8.6724],
+  ["Cartagena, Spain", 37.6257, -0.9966],
+  ["East Sussex, UK", 50.9316, 0.2106],
+  ["Prague, Czech Republic", 50.0755, 14.4378],
+  ["Madrid, Spain / Virtual", 40.4168, -3.7038]
+];
